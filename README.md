fa26e8d9dcf02e7943f8141dad5f0814ee22d845
Matthew Secrist <matthew@matthewsecrist.net>
2020-10-17 13:38:26 -0500

    Publish to hex

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 [![Coverage Status](https://coveralls.io/repos/github/matthewsecrist/boom/badge.svg)](https://coveralls.io/github/matthewsecrist/boom)
 [![Actions Status](https://github.com/matthewsecrist/boom/workflows/test/badge.svg)](https://github.com/matthewsecrist/boom/actions)
+[![Hex](https://img.shields.io/hexpm/v/boom?style=flat-square)](https://hex.pm/packages/boom)
 
 # Boom
 
-`Boom` is inspired by the NodeJS package `@hapi/boom`
-
-It allows you to respond with json errors easily.
+`Boom` is inspired by the NodeJS package `@hapi/boom`, allowing you to respond with json errors easily.
--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Boom.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       name: "Boom",
       source_url: "https://github.com/matthewsecrist/boom",
@@ -28,6 +30,19 @@ defmodule Boom.MixProject do
       {:plug_cowboy, "~> 2.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.23.0", only: :dev},
       {:inner_cotton, "~> 0.6", only: [:dev, :test]}
+    ]
+  end
+
+  defp description() do
+    "`Boom` is inspired by the NodeJS package `@hapi/boom`, allowing you to respond with json errors easily."
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/matthewsecrist/boom"}
     ]
   end
 end
